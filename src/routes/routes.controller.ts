433dc873688a553a4ac2a6f240b90037d8451a1f
lilianmartinsfritzen <liliancmartinsfritzen@gmail.com>
2023-08-24 01:35:39 +0000

    feature(back-tracking): route created with origin and destination of the route

--- src/routes/routes.controller.ts
+++ src/routes/routes.controller.ts
@@ -1,11 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { RoutesService } from './routes.service';
 import { CreateRouteDto } from './dto/create-route.dto';
 import { UpdateRouteDto } from './dto/update-route.dto';
 
 @Controller('routes')
 export class RoutesController {
-  constructor(private readonly routesService: RoutesService) {}
+  constructor(private readonly routesService: RoutesService) { }
 
   @Post()
   create(@Body() createRouteDto: CreateRouteDto) {
@@ -19,7 +27,7 @@ export class RoutesController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.routesService.findOne(+id);
+    return this.routesService.findOne(id);
   }
 
   @Patch(':id')
